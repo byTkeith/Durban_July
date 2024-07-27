b3b59715414320baadc1678fe8b2b0903732e3e3
Tendai <youremail@example.com>
2024-07-27 15:05:42 +0200

    added function to the buttons

--- AnotherOne1/Form1.Designer.cs
+++ AnotherOne1/Form1.Designer.cs
@@ -29,23 +29,23 @@
         private void InitializeComponent()
         {
             grpHorse = new GroupBox();
-            grpRoaster = new GroupBox();
-            btnPredictWinner = new Button();
-            btnAdd = new Button();
-            txtRoaster = new TextBox();
-            tex = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
             label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            txtTime3 = new TextBox();
+            txtTime2 = new TextBox();
+            txtTime1 = new TextBox();
+            txtAge = new TextBox();
+            txtHeight = new TextBox();
+            txtName = new TextBox();
+            btnAdd = new Button();
+            grpRoaster = new GroupBox();
+            txtRoaster = new TextBox();
+            btnPredictWinner = new Button();
             grpHorse.SuspendLayout();
             grpRoaster.SuspendLayout();
             SuspendLayout();
@@ -59,12 +59,12 @@
             grpHorse.Controls.Add(label3);
             grpHorse.Controls.Add(label2);
             grpHorse.Controls.Add(label1);
-            grpHorse.Controls.Add(textBox6);
-            grpHorse.Controls.Add(textBox5);
-            grpHorse.Controls.Add(textBox4);
-            grpHorse.Controls.Add(textBox3);
-            grpHorse.Controls.Add(textBox2);
-            grpHorse.Controls.Add(tex);
+            grpHorse.Controls.Add(txtTime3);
+            grpHorse.Controls.Add(txtTime2);
+            grpHorse.Controls.Add(txtTime1);
+            grpHorse.Controls.Add(txtAge);
+            grpHorse.Controls.Add(txtHeight);
+            grpHorse.Controls.Add(txtName);
             grpHorse.Controls.Add(btnAdd);
             grpHorse.Location = new Point(24, 69);
             grpHorse.Name = "grpHorse";
@@ -73,24 +73,110 @@
             grpHorse.TabStop = false;
             grpHorse.Text = "Horse";
             // 
-            // grpRoaster
+            // label7
             // 
-            grpRoaster.Controls.Add(txtRoaster);
-            grpRoaster.Location = new Point(336, 69);
-            grpRoaster.Name = "grpRoaster";
-            grpRoaster.Size = new Size(452, 272);
-            grpRoaster.TabIndex = 1;
-            grpRoaster.TabStop = false;
-            grpRoaster.Text = "Roaster";
+            label7.AutoSize = true;
+            label7.Location = new Point(19, 269);
+            label7.Name = "label7";
+            label7.Size = new Size(42, 15);
+            label7.TabIndex = 13;
+            label7.Text = "Time 3";
             // 
-            // btnPredictWinner
+            // label6
             // 
-            btnPredictWinner.Location = new Point(106, 399);
-            btnPredictWinner.Name = "btnPredictWinner";
-            btnPredictWinner.Size = new Size(108, 23);
-            btnPredictWinner.TabIndex = 2;
-            btnPredictWinner.Text = "Predict Winner";
-            btnPredictWinner.UseVisualStyleBackColor = true;
+            label6.AutoSize = true;
+            label6.Location = new Point(19, 230);
+            label6.Name = "label6";
+            label6.Size = new Size(42, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Time 2";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(19, 194);
+            label5.Name = "label5";
+            label5.Size = new Size(42, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Time 1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(19, 141);
+            label4.Name = "label4";
+            label4.Size = new Size(135, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Enter the last three races";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 103);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Height:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Age:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Name:";
+            // 
+            // txtTime3
+            // 
+            txtTime3.Location = new Point(82, 261);
+            txtTime3.Name = "txtTime3";
+            txtTime3.Size = new Size(122, 23);
+            txtTime3.TabIndex = 6;
+            // 
+            // txtTime2
+            // 
+            txtTime2.Location = new Point(82, 222);
+            txtTime2.Name = "txtTime2";
+            txtTime2.Size = new Size(122, 23);
+            txtTime2.TabIndex = 5;
+            // 
+            // txtTime1
+            // 
+            txtTime1.Location = new Point(82, 186);
+            txtTime1.Name = "txtTime1";
+            txtTime1.Size = new Size(122, 23);
+            txtTime1.TabIndex = 4;
+            // 
+            // txtAge
+            // 
+            txtAge.Location = new Point(71, 66);
+            txtAge.Name = "txtAge";
+            txtAge.Size = new Size(122, 23);
+            txtAge.TabIndex = 3;
+            // 
+            // txtHeight
+            // 
+            txtHeight.Location = new Point(71, 103);
+            txtHeight.Name = "txtHeight";
+            txtHeight.Size = new Size(122, 23);
+            txtHeight.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(67, 37);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(200, 23);
+            txtName.TabIndex = 1;
             // 
             // btnAdd
             // 
@@ -102,6 +188,16 @@
             btnAdd.UseVisualStyleBackColor = true;
             btnAdd.Click += btnAdd_Click;
             // 
+            // grpRoaster
+            // 
+            grpRoaster.Controls.Add(txtRoaster);
+            grpRoaster.Location = new Point(336, 69);
+            grpRoaster.Name = "grpRoaster";
+            grpRoaster.Size = new Size(452, 272);
+            grpRoaster.TabIndex = 1;
+            grpRoaster.TabStop = false;
+            grpRoaster.Text = "Roaster";
+            // 
             // txtRoaster
             // 
             txtRoaster.Location = new Point(18, 37);
@@ -110,110 +206,14 @@
             txtRoaster.Size = new Size(414, 208);
             txtRoaster.TabIndex = 0;
             // 
-            // tex
+            // btnPredictWinner
             // 
-            tex.Location = new Point(67, 37);
-            tex.Name = "tex";
-            tex.Size = new Size(200, 23);
-            tex.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(71, 103);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(122, 23);
-            textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(71, 66);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(122, 23);
-            textBox3.TabIndex = 3;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(82, 186);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(122, 23);
-            textBox4.TabIndex = 4;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(82, 222);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(122, 23);
-            textBox5.TabIndex = 5;
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(82, 261);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(122, 23);
-            textBox6.TabIndex = 6;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(19, 37);
-            label1.Name = "label1";
-            label1.Size = new Size(42, 15);
-            label1.TabIndex = 7;
-            label1.Text = "Name:";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(19, 74);
-            label2.Name = "label2";
-            label2.Size = new Size(31, 15);
-            label2.TabIndex = 8;
-            label2.Text = "Age:";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(19, 103);
-            label3.Name = "label3";
-            label3.Size = new Size(46, 15);
-            label3.TabIndex = 9;
-            label3.Text = "Height:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(19, 141);
-            label4.Name = "label4";
-            label4.Size = new Size(135, 15);
-            label4.TabIndex = 10;
-            label4.Text = "Enter the last three races";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(19, 194);
-            label5.Name = "label5";
-            label5.Size = new Size(42, 15);
-            label5.TabIndex = 11;
-            label5.Text = "Time 1";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(19, 230);
-            label6.Name = "label6";
-            label6.Size = new Size(42, 15);
-            label6.TabIndex = 12;
-            label6.Text = "Time 2";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(19, 269);
-            label7.Name = "label7";
-            label7.Size = new Size(42, 15);
-            label7.TabIndex = 13;
-            label7.Text = "Time 3";
+            btnPredictWinner.Location = new Point(106, 399);
+            btnPredictWinner.Name = "btnPredictWinner";
+            btnPredictWinner.Size = new Size(108, 23);
+            btnPredictWinner.TabIndex = 2;
+            btnPredictWinner.Text = "Predict Winner";
+            btnPredictWinner.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -246,11 +246,11 @@
         private Label label3;
         private Label label2;
         private Label label1;
-        private TextBox textBox6;
-        private TextBox textBox5;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
-        private TextBox tex;
+        private TextBox txtTime3;
+        private TextBox txtTime2;
+        private TextBox txtTime1;
+        private TextBox txtAge;
+        private TextBox txtHeight;
+        private TextBox txtName;
     }
 }
